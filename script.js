cc9bc5113fa6e51d5986b8159a4d4c2718547e5d
Zainab322 <zainabrashid322@gmail.com>
2025-02-03 00:01:37 +0500

    Added files for website

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to Asterra Cars!");
+
+    document.querySelector(".search-btn").addEventListener("click", function () {
+        alert("Search feature coming soon!");
+    });
+
+    document.querySelector(".logo").addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+
+});
